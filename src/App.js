6b8fa11d07c0d6b64e1a28960ff7d0363e013270
David Moreno <moreno@rushstreetinteractive.com>
2023-03-20 15:53:37 -0500

    Rudux hooks and middlewares

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { useEffect } from 'react';
-import { connect } from 'react-redux';
 import { Col } from 'antd';
 import PokemonList from './components/PokemonList';
 import Searcher from './components/Searcher';
 import { getPokemon } from './api';
-import { setPokemons as setPokemonsActions } from './actions';
+import { setPokemons } from './actions';
 import logo from './statics/logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App({ pokemons, setPokemons }) {
-  console.log("🚀 ~ file: App.js:12 ~ App ~ pokemons:", pokemons)
-
+// OLD REDUX WAY
+/* function App({ pokemons, setPokemons }) {
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonRes = await getPokemon();
@@ -41,4 +40,32 @@ const mapDispatchToProps = (dispatch) => ({
   setPokemons: value => dispatch(setPokemonsActions(value))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App); */
+
+function App() {
+  const pokemons = useSelector(state => state.pokemons);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonRes = await getPokemon();
+      dispatch(setPokemons(pokemonRes));
+    }
+    fetchPokemons();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="App">
+      <Col span={4} offset={10} >
+        <img src={logo} alt="Pokedux" />
+      </Col>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      <PokemonList pokemons={pokemons} />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { pokemonsReducer } from './reducers/pokemons';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
+import { featuring, logger } from './middlewares';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(pokemonsReducer);
+const composedEnhancer = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
+)
+
+const store = createStore(
+  pokemonsReducer,
+  composedEnhancer
+);
 
 root.render(
   <React.StrictMode>
